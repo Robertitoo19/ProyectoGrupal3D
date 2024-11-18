23bcd3ff11769cf4d318cf9e36479475ffc5cc00
Robertitoo19 <robertito191103@gmail.com>
2024-11-18 21:19:36 +0100

    dsa

--- Assets/ROBERTO/Scripts/ManualGun.cs
+++ Assets/ROBERTO/Scripts/ManualGun.cs
@@ -6,14 +6,20 @@ using UnityEngine;
 public class ManualGun : MonoBehaviour
 {
     [SerializeField] private ArmaSO myData;
+
+    private Camera cam;
     void Start()
     {
-        
+        cam = Camera.main;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hitInfo, myData.attackDistance))
+            {
+                Debug.Log(hitInfo.transform.name);
+            }
+        }
     }
 }
