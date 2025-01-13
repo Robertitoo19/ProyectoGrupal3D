2cc51a150767a99a52a929cf91d65bc5d47ebde5
Robertitoo19 <robertito191103@gmail.com>
2025-01-13 20:03:42 +0100

    ñlk

--- Assets/ROBERTO/Scripts/ManualGun.cs
+++ Assets/ROBERTO/Scripts/ManualGun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering.VirtualTexturing;
@@ -11,22 +12,90 @@ public class ManualGun : MonoBehaviour
     private Camera cam;
 
     [SerializeField] ParticleSystem particles;
+
+    [Header("-----Sistema Recarga-----")]
+
+    [SerializeField] private TMP_Text txtCurrentAmmo;
+    [SerializeField] private TMP_Text txtCurrentChamber;
+
+    private int currentAmmo;
+    private int currentChamber;
+
+    private float reloadTime = 1.5f;
+    private bool isReloading = false;
+    private Animator anim;
+
     void Start()
     {
         cam = Camera.main;
+        anim = GetComponent<Animator>();
+
+        currentAmmo = myData.bullets;
+        currentChamber = myData.chamberBullets;
+
+        txtCurrentAmmo.text = currentAmmo.ToString();
+        txtCurrentChamber.text = currentChamber.ToString();
+    }
+    private void OnEnable()
+    {
+        isReloading = false;
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (isReloading)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < myData.bullets)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0) && currentAmmo >0)
         {
             particles.Play();
+            currentAmmo--;
+            txtCurrentAmmo.text = currentAmmo.ToString();
+
             if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hitInfo, myData.attackDistance))
             {
-                if (hitInfo.transform.CompareTag("EnemyPart"))
+                if (hitInfo.transform.TryGetComponent(out EnemyPart enemyPartScript))
                 {
-                    hitInfo.transform.GetComponent<EnemyPart>().ReceiveDamageEnemy(myData.attackDamage);
+                    enemyPartScript.ReceiveDamageEnemy(myData.attackDamage);
                 }
             }
+        }
+    }
+    private IEnumerator Reload()
+    {
+        int emptys = myData.bullets - currentAmmo;
+
+        if (currentChamber >= emptys) 
+        {
+            isReloading = true;
+
+            yield return new WaitForSeconds(reloadTime);
+
+            currentAmmo = myData.bullets;
+            currentChamber -= emptys;
+
+            txtCurrentAmmo.text = currentAmmo.ToString();
+            txtCurrentChamber.text = currentChamber.ToString();
+
+            isReloading = false;
+        }
+        else
+        {
+            currentAmmo = currentAmmo + currentChamber;
+            currentChamber = 0;
+
+            txtCurrentAmmo.text = currentAmmo.ToString();
+            txtCurrentChamber.text = currentChamber.ToString();
         }
     }
 }
