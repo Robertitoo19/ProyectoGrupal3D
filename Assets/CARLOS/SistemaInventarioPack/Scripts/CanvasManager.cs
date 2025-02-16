71038020bb45b6e5343f8bc2c5471406a8124272
Robertitoo19 <robertito191103@gmail.com>
2025-02-16 23:43:09 +0100

    dfhge

--- Assets/CARLOS/SistemaInventarioPack/Scripts/CanvasManager.cs
+++ Assets/CARLOS/SistemaInventarioPack/Scripts/CanvasManager.cs
@@ -7,58 +7,58 @@ using UnityEngine.SceneManagement;
 
 public class CanvasManager : MonoBehaviour
 {
-    [SerializeField]
-    private GameManagerSO gM;
+    //[SerializeField]
+    //private GameManagerSO gM;
 
 
-    [SerializeField]
-    private GameObject mainMenu;
+    //[SerializeField]
+    //private GameObject mainMenu;
 
-    [SerializeField]
-    private TMP_Text textoMonedas;
-    // Start is called before the first frame update
-    void Start()
-    {
+    //[SerializeField]
+    //private TMP_Text textoMonedas;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))
-        {
-            mainMenu.SetActive(!mainMenu.activeSelf);
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if(Input.GetKeyDown(KeyCode.Escape))
+    //    {
+    //        mainMenu.SetActive(!mainMenu.activeSelf);
+    //    }
+    //}
 
-    public void OnSaveButtonClicked()
-    {
+    //public void OnSaveButtonClicked()
+    //{
         
-        SaveSystem.Save(gM);
-    }
+    //    SaveSystem.Save(gM);
+    //}
 
-    public void OnLoadButtonClicked()
-    {
-        StartCoroutine(LoadSceneAndData());
+    //public void OnLoadButtonClicked()
+    //{
+    //    StartCoroutine(LoadSceneAndData());
 
-    }
-    private IEnumerator LoadSceneAndData()
-    {
-        PersistentData dataToLoad = SaveSystem.Load(); //Primero cargo los datos
+    //}
+    //private IEnumerator LoadSceneAndData()
+    //{
+    //    PersistentData dataToLoad = SaveSystem.Load(); //Primero cargo los datos
 
-        gM.Items = dataToLoad.Items; //Y tengo en cuenta aquellos antes de cargar el nivel.
+    //    gM.Items = dataToLoad.Items; //Y tengo en cuenta aquellos antes de cargar el nivel.
 
-        AsyncOperation operation = SceneManager.LoadSceneAsync(0);
-        yield return new WaitUntil(() => operation.isDone);
+    //    AsyncOperation operation = SceneManager.LoadSceneAsync(0);
+    //    yield return new WaitUntil(() => operation.isDone);
 
-        foreach(ItemSO item in dataToLoad.PersistentItems) 
-        {
-            gM.NewItem(item);
+    //    foreach(ItemSO item in dataToLoad.PersistentItems) 
+    //    {
+    //        gM.NewItem(item);
 
-        }
-        textoMonedas.text = "Coins: " + dataToLoad.Monedas;
+    //    }
+    //    textoMonedas.text = "Coins: " + dataToLoad.Monedas;
 
-        gM.currentPlayer.transform.position = new Vector3(dataToLoad.LastPositionX, dataToLoad.LastPositionY);
+    //    gM.currentPlayer.transform.position = new Vector3(dataToLoad.LastPositionX, dataToLoad.LastPositionY);
 
-    }
+    //}
 }
